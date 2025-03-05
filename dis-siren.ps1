ad71a603b48ba6ccd2048e402df2d3c89e278072
Jensone <hello@jensone.com>
2025-03-05 15:16:19 +0100

    Implement initial version of Dis-Siren script for company search based on SIREN number with basic validation and API interaction

---
+++ dis-siren.ps1
@@ -0,0 +1,84 @@
+# Programme Dis-Siren
+# Application de recherche d'entreprises basé sur le numéro SIREN
+
+# Variables
+$nombreTentatives = 0
+$delaiAttente = 3
+$urlAPI = "https://recherche-entreprises.api.gouv.fr/search?q="
+
+Write-Host "
+▗▄▄▄ ▗▄▄▄▖ ▗▄▄▖     ▗▄▄▖▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖▗▖  ▗▖
+▐▌  █  █  ▐▌       ▐▌     █  ▐▌ ▐▌▐▌   ▐▛▚▖▐▌
+▐▌  █  █   ▝▀▚▖     ▝▀▚▖  █  ▐▛▀▚▖▐▛▀▀▘▐▌ ▝▜▌
+▐▙▄▄▀▗▄█▄▖▗▄▄▞▘    ▗▄▄▞▘▗▄█▄▖▐▌ ▐▌▐▙▄▄▖▐▌  ▐▌
+"
+Write-Host "beta1.0.0 by @--- • 2025"
+Write-Host "Vous pouvez quitter à tout moment avec Ctrl+C"
+
+function Get-CompanyInfo {
+    # Validation du SIREN
+    Write-Host "Entrez le numéro SIREN de l'entreprise"
+    $siren = Read-Host "SIREN (9 chiffres)"
+
+    if ($siren -eq "")
+    {
+        Write-Host "Veuillez entrer un numéro SIREN"
+        Get-CompanyInfo
+    }    
+
+    if ($siren.Length -ne 9 -or $siren -notmatch "^[0-9]+$")
+    {
+        Write-Host "Le numéro SIREN doit contenir 9 chiffres"
+        Get-CompanyInfo
+    }    
+    
+    if ($siren.Substring(0, 1) -eq "0")
+    {
+        Write-Host "Le numéro SIREN ne peut pas commencer par 0"
+        Get-CompanyInfo
+    }    
+
+    # Préparation de la requête
+    $url = $urlAPI + $siren
+    $conn = Test-Connection -ComputerName "recherche-entreprises.api.gouv.fr" -Count 1 -Quiet
+
+    if (-not $conn)
+    {
+        Write-Host "Erreur: Pas de connexion Internet"
+    } else {
+        # TANT QUE nombreTentatives < 2
+        while ($nombreTentatives -lt 2) {
+            #INCRÉMENTER nombreTentatives
+            $nombreTentatives++
+
+            Write-Host "Récupération des données en cours..."
+            
+            # Requêtes vers l'API
+            $response = Invoke-RestMethod -Method 'Get' -Uri $url 
+            
+            if ($response.results[0].nom_complet -eq "") {
+                Write-Host "Le numéro SIREN n'existe pas"
+                Get-CompanyInfo
+            }
+            
+            sleep(3)
+
+            $nom_raison_sociale = $response.results[0].nom_raison_sociale
+            $date_creation = $response.results[0].date_creation
+            $activite_principale = $response.results[0].activite_principale
+            
+            # GENERATION DU DOCUMENT, ENVOI DU MAIL
+            Write-Host $nom_raison_sociale, $date_creation, $activite_principale
+            
+            Write-Host "Merci d'avoir utilisé Dis-Siren, à bientôt !"
+            exit
+        }
+        
+        
+            
+    }
+
+
+}
+
+Get-CompanyInfo
